80f81521556bc53d4a4eaaac6e3ce8e7a2c9a78d
bvpav <78971592+bvpav@users.noreply.github.com>
2024-11-13 12:48:17 +0200

    init

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "selector",
+  theme: {
+    extend: {
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        scroll: "scroll 25s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
